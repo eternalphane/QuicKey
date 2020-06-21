986a618d5ed7ff9e11d848065671f6633a267461
John Dunning <fw@johndunning.com>
2020-06-20 17:46:06 -0700

    Don't focus item under mouse when popup is opened
    Reset mouseMoveCount in the list items when the item is changed or the list is hidden, so that the item won't get selected if it's opened under the mouse.
    Focus the searchbox when the popup window is shown, in case something else had focus the last time it was shown.
    Filter out incognito windows from getLastWindow().
    Catch errors in popupWindow.show() and hide().
    ES6ify results-list.js.

--- src/js/popup/app.js
+++ src/js/popup/app.js
@@ -86,6 +86,7 @@ define("popup/app", [
 		gotMRUKey: false,
 		mruModifier: "Alt",
 		resultsList: null,
+		searchBox: null,
 		settings: settings.getDefaults(),
 		settingsPromise: null,
 
@@ -716,6 +717,12 @@ define("popup/app", [
 				// selected after tabs are loaded
 			this.openedForSearch = focusSearch;
 
+				// regardless of whether the first item is selected or not, set
+				// the focus to the searchbox, in case the last time the window
+				// was open the user had clicked somewhere else.  without this,
+				// the focus would still be on that other element.
+			this.searchBox.focus();
+
 				// set these so that when the modifier key is released (it had
 				// to have been pressed for showWindow() to be called), it
 				// activates the selected item
@@ -758,6 +765,9 @@ define("popup/app", [
 
 
 		handleListRef: handleRef("resultsList"),
+
+
+		handleSearchBoxRef: handleRef("searchBox"),
 
 
 		onTabRemoved: function()
@@ -871,6 +881,7 @@ define("popup/app", [
 			return <div className={this.props.platform}>
 				<SearchBox
 					autoFocus
+					ref={this.handleSearchBoxRef}
 					mode={this.mode}
 					forceUpdate={this.forceUpdate}
 					selectAll={this.selectAllSearchBoxText}
@@ -890,6 +901,7 @@ define("popup/app", [
 					itemComponent={ResultsListItem}
 					mode={this.mode}
 					query={query}
+					visible={this.visible}
 					selectedIndex={selected}
 					setSelectedIndex={this.setSelectedIndex}
 					onItemClicked={this.openItem}
